a2622955ff8c89b7477503745b0c6e4adb959358
Tonycs <lam.tony540@gmail.com>
2026-07-20 17:18:00 -0400

    fix(app): widen reset and forgot password containers
    Signed-off-by: Tonycs <lam.tony540@gmail.com>

--- app/src/routes/forgot-password.tsx
+++ app/src/routes/forgot-password.tsx
@@ -11,7 +11,7 @@ function RouteComponent() {
 
   return (
     <div className="flex min-h-[calc(100svh_-_70px)] flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
-      <div className="flex max-w-[1280px] flex-col gap-6">
+      <div className="flex w-full max-w-[1280px] flex-col gap-6">
         <ForgotPasswordForm />
       </div>
     </div>
--- app/src/routes/reset-password.tsx
+++ app/src/routes/reset-password.tsx
@@ -8,7 +8,7 @@ export const Route = createFileRoute("/reset-password")({
 function RouteComponent() {
   return (
     <div className="flex min-h-[calc(100svh_-_70px)] flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
-      <div className="flex max-w-[1280px] flex-col gap-6">
+      <div className="flex w-full max-w-[1280px] flex-col gap-6">
         <ResetPasswordForm />
       </div>
     </div>
